592ca7af7fec6f85c52bcbe35ccabe2041f75d7c
Jack_Wang <56127817+Outsider565@users.noreply.github.com>
2022-04-07 21:08:21 +0800

    Add implementation of task1

--- lab5/task0/main.cpp
+++ lab5/task0/main.cpp
@@ -4,9 +4,9 @@ int main(){
     auto display = Display();
     display.put_string(display.get_cols()/2, display.get_lines()/2, "Hello, world!");
     // TODO: put a "I love OOP" on the bottom of the screen
-    // Your code here
-
-    
+    // Your code starts here
+    display.put_string(display.get_cols()/2, display.get_lines() - 1, "I love OOP");
+    // Your code ends here
     display.refresh();
     display.get_char(); // Wait until press the key
     return 0;
