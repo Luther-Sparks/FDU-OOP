9fa008a7fad5031880404e50f192efb8e6f4a04c
Jack_Wang <56127817+Outsider565@users.noreply.github.com>
2022-04-27 22:36:00 +0800

    Update display

--- lab4_2/lib/display.h
+++ lab4_2/lib/display.h
@@ -72,7 +72,7 @@ class Display {
     // e.g.
     // [3.23] string
     // screen
-    void log(std::string str = "");
+    void log(std::string str = "", bool print_screen = true);
 
    private:
     int lines, cols;
@@ -82,4 +82,5 @@ class Display {
     std::ofstream* plog_file;
     WINDOW* win;
     std::chrono::time_point<std::chrono::system_clock> time_start;
+    void assert(bool condition, std::string message);
 };
