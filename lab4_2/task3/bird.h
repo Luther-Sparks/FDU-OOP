0e6640b963eb12242c28260588c12854c0c5a6eb
Jack_Wang <56127817+Outsider565@users.noreply.github.com>
2022-04-28 12:56:43 +0800

    Add task3

---
+++ lab4_2/task3/bird.h
@@ -0,0 +1,27 @@
+#ifndef BIRD_GUARD
+#define BIRD_GUARD
+#include <utility>
+#include "../game/object.h"
+class Bird : public Object {
+public:
+    Bird() = default;
+    Bird(double x, double y, double vx, double vy, double ax, double ay);
+    Bird& operator=(const Bird&) = default;
+    ~Bird() = default;
+
+    void draw(Display& display) const override;
+    void tick(int key) override;
+    bool is_inside(int x, int y) const override;
+    Type type() const override { return Type::Bird; }
+    bool detect_collide(Object& obj) override;
+    std::pair<int, int> get_pos() const;
+    std::pair<double, double> get_v() const;
+    std::pair<double, double> get_a() const;
+    void set_pos(int _x, int _y);
+    void set_v(double _vx, double _vy);
+    void set_a(double _ax, double _ay);
+
+private:
+    double x, y, vx, vy, ax, ay;
+};
+#endif
