cced8d85e8caf1e10ec7e1f9d116ea5aee2356e1
Jack_Wang <56127817+Outsider565@users.noreply.github.com>
2022-05-07 09:27:27 +0800

    修改了注释

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -1,10 +1,11 @@
-#include "Expression.h"
 #include <iostream>
 
-int main(){
-    auto expr = sin(5.23333*X());
-    std::cout<< expr.toString()<<std::endl;
+#include "Expression.h"
+
+int main() {
+    auto expr = sin(5.23333 * X()) + 5 * X() * X() - cos(X());
+    std::cout << expr.toString() << std::endl;
     auto diff = expr.getDerivative();
-    std::cout<<diff.toString()<<std::endl;
-    std::cout<<diff.evaluate(1.0)<<std::endl;
+    std::cout << diff.toString() << std::endl;
+    std::cout << diff.evaluate(1.0) << std::endl;
 }
